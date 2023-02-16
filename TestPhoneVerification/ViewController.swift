2834481b12d736febbe2042b674bc67fde058207
Ergashev Odiljon <oaergashev@gmail.com>
2023-02-16 12:58:38 +0500

    Initial Commit

---
+++ TestPhoneVerification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPhoneVerification
+//
+//  Created by Odiljon Ergashev on 2023/02/16.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var customWebView: CustomWKWebView!
+    
+    let url = "http://52.79.159.186:8080/seyfert/phone/main?type=REG"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        customWebView.loadUrl(url)
+    }
+
+
+}
+
